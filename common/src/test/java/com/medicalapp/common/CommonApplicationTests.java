465f74464dc1071a7ef7ba9e98ef271efe587403
SebastianHV <sebastian.hdz.val@gmail.com>
2025-08-07 00:32:46 -0600

    feat: initial multi-module project setup with auth-service

---
+++ common/src/test/java/com/medicalapp/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalapp.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
